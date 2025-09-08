1180026cf04351a574e4c0e94bdf78f522f1cdb5
haengjoo123 <gudwns999999@gmail.com>
2025-09-09 08:54:03 +0900

    ver1.22

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="text-center">
           <p className="text-sm">
-            &copy; 2024 인제대학교 와이파이 제보 시스템. All rights reserved.
+            &copy; 2025 인제대학교 와이파이 제보 시스템. All rights reserved.
           </p>
           <p className="text-xs mt-2 text-secondary-400">
             캠퍼스 내 와이파이 문제 개선을 위한 학생 참여 플랫폼
