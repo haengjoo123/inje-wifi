4c353a32d3bb5388258e5b74ec1d3cd706a11364
haengjoo123 <gudwns999999@gmail.com>
2025-09-08 21:43:20 +0900

    ver1

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,8 +1,13 @@
 /// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
 
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string;
   readonly VITE_APP_TITLE?: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  readonly MODE: string;
 }
 
 interface ImportMeta {
