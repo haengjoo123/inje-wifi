d941f5690411490fbb6db87d70e5a85b4c1a9732
haengjoo123 <gudwns999999@gmail.com>
2025-09-08 21:36:40 +0900

    reroll

--- client/src/types/global.d.ts
+++ client/src/types/global.d.ts
@@ -1,13 +1,4 @@
-// Vite 환경 변수 타입 정의
-interface ImportMetaEnv {
-  readonly VITE_API_URL?: string;
-  readonly VITE_APP_TITLE?: string;
-  // 필요한 환경 변수들을 여기에 추가
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+/// <reference types="vite/client" />
 
 declare module 'aria-query' {
   const ariaQuery: any;
